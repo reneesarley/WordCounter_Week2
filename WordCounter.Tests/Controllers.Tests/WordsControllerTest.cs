656129c0e9c66d632b118e55b7e142fce2225a49
Renee Sarley <reneesarley@gmail.com>
2018-07-06 14:38:26 -0700

    add test method for WordScorer

---
+++ WordCounter.Tests/Controllers.Tests/WordsControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WordCounter.Controllers;
+using WordCounter;
+using WordCounter.Models;
+
+namespace WordCounter.Tests.Controllers.Tests
+{
+    [TestClass]
+    public class WordsControllerTest
+    {
+        [TestMethod]
+        public void WordScorer_ReturnsCorrectView_True()
+        {
+            //Arrange
+            WordsController controller = new WordsController();
+
+            //Act
+            ActionResult wordScorerView = controller.WordScorer();
+
+            //Assert
+            Assert.IsInstanceOfType(wordScorerView, typeof(ViewResult));
+        }
+    }
+}
