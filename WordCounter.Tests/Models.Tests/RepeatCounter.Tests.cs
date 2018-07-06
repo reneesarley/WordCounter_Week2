4c14022c1cc9eebe77799cf35c1797b17cfd05c8
Renee Sarley <reneesarley@gmail.com>
2018-07-06 10:05:59 -0700

    add model and model test for RepeatCounter Class

---
+++ WordCounter.Tests/Models.Tests/RepeatCounter.Tests.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using WordCounter;
+using WordCounter.Models;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordCounter.Tests
+
+{
+    [TestClass]
+    public class RepeatCounterTests
+    {
+        [TestMethod]
+        public void GetWordCounter_ReturnCurrentValueOfWordCounter_Int()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+
+            //Act
+            int result = newRepeatCounter.GetWordCounter();
+
+            //Assert∫
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void SetGetWordToFind_SetAndGetValueOfWordToFind_String()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            string testWord = "back";
+
+            //Act
+            newRepeatCounter.SetWordToFind(testWord);
+            string resultWord = newRepeatCounter.GetWordToFind();
+
+            //Assert
+            Assert.AreEqual(resultWord, testWord);
+        }
+
+        [TestMethod]
+        public void SetGetStringToSearch_SetAndGetValueOfStringToSearch_String()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            string testString = "Bob fell on his back";
+
+            //Act
+            newRepeatCounter.SetStringToSearch(testString);
+            string resultString = newRepeatCounter.GetStringToSearch();
+
+            //Assert
+            Assert.AreEqual(resultString, testString);
+        }
+
+        [TestMethod]
+        public void UpdateWordCounter_ReturnOneWhenWordsMatch_Int()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            string testWord = "back";
+            string testSentence = "Bob fell on his back";
+
+            //Act
+            newRepeatCounter.SetWordToFind(testWord);
+            newRepeatCounter.SetStringToSearch(testSentence);
+            newRepeatCounter.UpdateWordCounter();
+            int result = newRepeatCounter.GetWordCounter();
+
+            //Assert
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void UpdateWordCount_ReturnZeroWhenWordsDontMatch_Int()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            string testWord = "back";
+            string testSentence = "Bob fell backwards";
+
+            //Act
+            newRepeatCounter.SetWordToFind(testWord);
+            newRepeatCounter.SetStringToSearch(testSentence);
+            newRepeatCounter.UpdateWordCounter();
+            int result = newRepeatCounter.GetWordCounter();
+
+            //Assert
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void UpdateWordCounter_ReturnOneWhenWordWithPuncMatches_Int()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            string testWord = "back";
+            string testSentence = "Bob feel on his back.";
+
+            //Act
+            newRepeatCounter.SetWordToFind(testWord);
+            newRepeatCounter.SetStringToSearch(testSentence);
+            newRepeatCounter.UpdateWordCounter();
+            int result = newRepeatCounter.GetWordCounter();
+
+            //Assert
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void UpdateWordCounter_ConvertUpperCaseToLower_Int()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            string testWord = "back";
+            string testSentence = "Bob fell BACK";
+
+            //Act
+            newRepeatCounter.SetWordToFind(testWord);
+            newRepeatCounter.SetStringToSearch(testSentence);
+            newRepeatCounter.UpdateWordCounter();
+            int result = newRepeatCounter.GetWordCounter();
+
+            //Assert
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void UpdateWordCounter_ReturnOneDueToOneMatchingWord_Int()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            newRepeatCounter.SetStringToSearch("Bob fell on his back");
+            newRepeatCounter.SetWordToFind("back");
+            string testSentence = newRepeatCounter.GetStringToSearch();
+            string testWord = newRepeatCounter.GetWordToFind();
+
+
+            //Act
+            newRepeatCounter.UpdateWordCounter();
+            int result = newRepeatCounter.GetWordCounter();
+
+            //Assert
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void UpdateWordCounter_ReturnThreeDueToThreeMatchingWords_Int()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            newRepeatCounter.SetStringToSearch("Bob fell on his back but didn't hurt his back so he got back up");
+            newRepeatCounter.SetWordToFind("back");
+            string testSentence = newRepeatCounter.GetStringToSearch();
+            string testWord = newRepeatCounter.GetWordToFind();
+
+
+            //Act
+            newRepeatCounter.UpdateWordCounter();
+            int result = newRepeatCounter.GetWordCounter();
+
+            //Assert
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void UpdateWordCounter_ReturnZeroDueToNoMatchingWords_Int()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            newRepeatCounter.SetStringToSearch("Bob fell on his face");
+            newRepeatCounter.SetWordToFind("back");
+            string testSentence = newRepeatCounter.GetStringToSearch();
+            string testWord = newRepeatCounter.GetWordToFind();
+
+
+            //Act
+            newRepeatCounter.UpdateWordCounter();
+            int result = newRepeatCounter.GetWordCounter();
+
+            //Assert
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void StripPunctuation_RemoveAllPunctuation_String()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            string testWord = "back.";
+
+            //Act
+            string result = newRepeatCounter.StripPunctuation(testWord);
+
+            //Assert
+            Assert.AreEqual(result, "back");
+        }
+        [TestMethod]
+        public void StripPunctuation_OnlyRemovePunctuationAtEndOfWord_String()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            string testWord = "To-Do";
+
+            //Act
+            string result = newRepeatCounter.StripPunctuation(testWord);
+
+            //Assert
+            Assert.AreEqual(result, "To-Do");
+        }
+
+        [TestMethod]
+        public void VerifyJustOneWord_ReturnFalseIfSpacesAreFound_Boolen()
+        {
+            //Arange
+            RepeatCounter newRepeatCounter = new RepeatCounter();
+            string testWord = "Two Words";
+
+            //Act
+            newRepeatCounter.SetWordToFind(testWord);
+            bool result = newRepeatCounter.VerifyJustOneWord();
+
+            //Assert
+            Assert.AreEqual(result, false);
+
+        }
+    }
+}
---
+++ WordCounter/Models/RepeatCounter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordCounter.Models
+{
+    public class RepeatCounter
+    {
+        private int _wordCounter;
+        private string _wordToFind;
+        private string _stringToSearch;
+
+        public RepeatCounter()
+        {
+            _wordCounter = 0;
+        }
+
+        public int GetWordCounter()
+        {
+            return _wordCounter;
+        }
+
+        public void SetWordToFind(string inputtedWord)
+        {
+            _wordToFind = inputtedWord;
+        }
+
+        public string GetWordToFind()
+        {
+            return _wordToFind;
+        }
+
+        public void SetStringToSearch(string inputtedString)
+        {
+            _stringToSearch = inputtedString;
+        }
+        public string GetStringToSearch()
+        {
+            return _stringToSearch;
+        }
+
+        public void UpdateWordCounter()
+        {
+            List<string> wordsToCompare = new List<string>();
+            wordsToCompare = (new List<string>(_stringToSearch.Split(' ')));
+            for (int i = 0; i < wordsToCompare.Count; ++i)
+            {
+                if (StripPunctuation(wordsToCompare[i].ToLower()) == _wordToFind.ToLower())
+                {
+                    _wordCounter = ++_wordCounter;
+                }
+            }
+        }
+
+        public string StripPunctuation(string word)
+        {
+            var noPunctuationString = new StringBuilder();
+            for (int i = 0; i < word.Length; ++i)
+            {
+                if (!Char.IsPunctuation(word[i]) && i == word.Length - 1)
+                {
+                    noPunctuationString.Append(word[i]);
+                }
+                else if (i != word.Length - 1)
+                {
+                    noPunctuationString.Append(word[i]);
+                }
+            }
+            return noPunctuationString.ToString();
+        }
+        public bool VerifyJustOneWord()
+        {
+            if (_wordToFind.Contains(" "))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
