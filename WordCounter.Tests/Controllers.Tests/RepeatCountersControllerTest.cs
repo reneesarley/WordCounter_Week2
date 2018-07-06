4a5ea64193a38a368b6a935684681cd0cb61fd77
Renee Sarley <reneesarley@gmail.com>
2018-07-06 10:49:39 -0700

    add RepeatCountersController and tests, add Start view

---
+++ WordCounter.Tests/Controllers.Tests/RepeatCountersControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WordCounter.Controllers;
+using WordCounter;
+
+
+namespace WordCounter.Tests.Controllers.Tests
+{
+    [TestClass]
+    public class RepeatCountersControllerTest
+    {
+        [TestMethod]
+        public void Start_ReturnsCorrectView_True()
+        {
+            //Arrange
+            RepeatCountersController controller = new RepeatCountersController();
+
+            //Act
+            ActionResult startView = controller.Start();
+
+            //Assert
+            Assert.IsInstanceOfType(startView, typeof(ViewResult));
+        }
+    }
+}
