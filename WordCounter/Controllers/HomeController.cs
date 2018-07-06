bb7fe051d5b16d67bf8d9e22b31682fa9d263670
Renee Sarley <reneesarley@gmail.com>
2018-07-06 10:14:22 -0700

    add home controller and view, and add home controller test

--- WordCounter/Controllers/HomeController.cs
+++ WordCounter/Controllers/HomeController.cs
@@ -1,35 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace WordCounter.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        [HttpGet("/")]
+        public ActionResult Index()
         {
             return View();
-        }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
         }
     }
 }
